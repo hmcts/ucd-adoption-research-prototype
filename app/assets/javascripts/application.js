0e4d970ffecbb2088c2af09ae23f1acc4bcf15b9
Eilis van der Merwe <Eilis.vandermerwe@hmcts.net>
2019-11-15 13:32:33 +0000

    initating index, login, task list page and about the child journey

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,14 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+// Back links
+
+$("a.govuk-back-link").click(function() {
+    return false;
+});
+
+$("a.govuk-back-link").click(function(){
+  parent.history.back();
+  return false;
+});
--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Apply to adopt a child placed in your care prototype',
 
   // Default port that prototype runs on
   port: '3000',
